eeb57462cc1e54263ff06dbaf07921b35821b9a6
Kenneth Krogh Rodum <kennekro@gmail.com>
2019-03-14 12:47:00 +0100

    Initial project commit

---
+++ src/webparts/customDropdown/CustomDropdownWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'CustomDropdownWebPartStrings';
+import CustomDropdown from './components/CustomDropdown';
+import { ICustomDropdownProps } from './components/ICustomDropdownProps';
+
+export interface ICustomDropdownWebPartProps {
+  description: string;
+}
+
+export default class CustomDropdownWebPart extends BaseClientSideWebPart<ICustomDropdownWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ICustomDropdownProps > = React.createElement(
+      CustomDropdown,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/customDropdown/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICustomDropdownWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CustomDropdownWebPartStrings' {
+  const strings: ICustomDropdownWebPartStrings;
+  export = strings;
+}
