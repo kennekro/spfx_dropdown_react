5c7986a1db1f103994af621fe889abed42ee9b22
Kenneth Krogh Rodum <kennekro@gmail.com>
2019-03-14 13:33:50 +0100

    Added the Dropdown component and some logic to show and hide the 2nd dropdown and button

--- src/webparts/customDropdown/components/CustomDropdown.tsx
+++ src/webparts/customDropdown/components/CustomDropdown.tsx
@@ -2,23 +2,57 @@ import * as React from 'react';
 import styles from './CustomDropdown.module.scss';
 import { ICustomDropdownProps } from './ICustomDropdownProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const optionsCategory = [
+  'Cars', 'Food', 'Soccer team'
+]
+const optionsSoccerTeams = [
+  'AC Milan', 'Arsenal', 'Barcelona'
+]
 
 export default class CustomDropdown extends React.Component<ICustomDropdownProps, {}> {
+
+  state = {
+    selectedCategory: '',
+    selectedSoccerTeam: '',
+    showSoccerTeamDropdown: false,
+    url: ''
+  }
+
+  categoryChanged(newCategory) {
+    this.setState({
+      selectedCategory: newCategory.value
+    })
+    switch (newCategory.value){
+      case 'Soccer team':
+        this.setState({
+          showSoccerTeamDropdown: true
+        })
+        break
+      default: 
+        this.setState({
+          showSoccerTeamDropdown: false,
+          url: ''
+        })
+        break
+    }
+  }
+
+  soccerTeamChanged(newSoccerTeam) {
+    this,this.setState({
+      selectedSoccerTeam: newSoccerTeam.value,
+      url: 'https://www.' + newSoccerTeam.value.replace(/\s/g, "") + '.com'
+    })
+  }
+
   public render(): React.ReactElement<ICustomDropdownProps> {
     return (
       <div className={ styles.customDropdown }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+        <Dropdown className='categoryDropdown' options={optionsCategory} value={this.state.selectedCategory} onChange={(e) => this.categoryChanged(e)} placeholder='Select category' />
+        { this.state.showSoccerTeamDropdown ? <Dropdown className='soccerTeamDropdown' options={optionsSoccerTeams} value={this.state.selectedSoccerTeam} onChange={(e) => this.soccerTeamChanged(e)} placeholder='Select soccer team' /> : null }
+        { this.state.url != '' ? <a href={this.state.url}><div className={styles.selectedLink}>OK</div></a> : null }
       </div>
     );
   }
